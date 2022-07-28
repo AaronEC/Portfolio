0b62638b941ce8c8d004f3632989d5d7c862f8a5
AaronEC <aaron_cardwell@hotmail.com>
2022-07-28 11:38:27 -0700

    Added smooth collapse on header elements.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,128 +5,28 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("banner").style.fontSize = "10pt";
-	document.getElementById("para").style.display = "none";
-	document.getElementById("about").style.display = "none";
-	document.getElementById("chevron").style.display = "none";
-	document.getElementById("projects-label").style.display = "none";
+	document.getElementById("hide").className = 'hidden';
+	// document.getElementById("para").style.display = "none";
+	// document.getElementById("about").style.display = "none";
+	// document.getElementById("chevron").style.display = "none";
+	// document.getElementById("projects-label").style.display = "none";
+	showhide();
   } else {
     document.getElementById("banner").style.fontSize = "12pt";
-	document.getElementById("para").style.display = "block";
-	document.getElementById("about").style.display = "block";
-	document.getElementById("chevron").style.display = "block";
-	document.getElementById("projects-label").style.display = "block";
+	document.getElementById("hide").className = 'toggle-div';
+	// document.getElementById("para").style.display = "block";
+	// document.getElementById("about").style.display = "block";
+	// document.getElementById("chevron").style.display = "block";
+	// document.getElementById("projects-label").style.display = "block";
   }
 }
 
-(function($) {
 
-	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
-	});
+// Toggle show and hide divs
 
-	$(function() {
-
-		var $window = $(window),
-			$body = $('body');
-
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
-
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
-
-		// Banner.
-			var $banner = $('#banner');
-
-			if ($banner.length > 0) {
-
-				// IE fix.
-					if (skel.vars.IEVersion < 12) {
-
-						$window.on('resize', function() {
-
-							var wh = $window.height() * 0.60,
-								bh = $banner.height();
-
-							$banner.css('height', 'auto');
-
-							window.setTimeout(function() {
-
-								if (bh < wh)
-									$banner.css('height', wh + 'px');
-
-							}, 0);
-
-						});
-
-						$window.on('load', function() {
-							$window.triggerHandler('resize');
-						});
-
-					}
-
-				// Video check.
-					var video = $banner.data('video');
-
-					if (video)
-						$window.on('load.banner', function() {
-
-							// Disable banner load event (so it doesn't fire again).
-								$window.off('load.banner');
-
-							// Append video if supported.
-								if (!skel.vars.mobile
-								&&	!skel.breakpoint('large').active
-								&&	skel.vars.IEVersion > 9)
-									$banner.append('<video autoplay loop><source src="' + video + '.mp4" type="video/mp4" /><source src="' + video + '.webm" type="video/webm" /></video>');
-
-						});
-
-				// More button.
-					$banner.find('.more')
-						.addClass('scrolly');
-
-			}
-
-		// Scrolly.
-			$('.scrolly').scrolly();
-
-		// Poptrox.
-			$window.on('load', function() {
-
-				var $thumbs = $('.thumbnails');
-
-				if ($thumbs.length > 0)
-					$thumbs.poptrox({
-						onPopupClose: function() { $body.removeClass('is-covered'); },
-						onPopupOpen: function() { $body.addClass('is-covered'); },
-						baseZIndex: 10001,
-						useBodyOverflow: false,
-						overlayColor: '#222226',
-						overlayOpacity: 0.75,
-						popupLoaderText: '',
-						fadeSpeed: 500,
-						usePopupDefaultStyling: false,
-						windowMargin: (skel.breakpoint('small').active ? 5 : 50)
-					});
-
-			});
-
-		// Initial scroll.
-			$window.on('load', function() {
-				$window.trigger('scroll');
-			});
-
-	});
-
-})(jQuery);
+function showhide() {
+	var x = document.querySelectorAll(".toggle-div");
+	for (var i = 0; i < x.length; i++) {
+	  x[i].classList.toggle('hidden');
+	}
+  }
